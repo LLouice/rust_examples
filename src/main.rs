d5bfc7f56767e0234fe78a10f37cf5188ed7a670
LLouice <g13920228592@gmail.com>
2025-04-13 22:21:11 +0800

    json to file & add anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use anyhow::Result;
+use std::fs::File;
 
 #[derive(Serialize, Deserialize)]
 struct Person {
@@ -20,10 +21,13 @@ fn typed_example() -> Result<()> {
             ]
         }"#;
 
-    // Parse the string of data into a Person object. This is exactly the
-    // same function as the one that produced serde_json::Value above, but
-    // now we are asking it for a Person as output.
+    // Parse the string of data into a Person object.
     let p: Person = serde_json::from_str(data)?;
+
+    // Serialize the Person object back to JSON
+    // Create and write directly to the file using serde_json::to_writer_pretty
+    let mut file = File::create("../Person.json")?;
+    serde_json::to_writer_pretty(&mut file, &p)?;
 
     // Do things just like with any other Rust data structure.
     println!("Please call {} at the number {}", p.name, p.phones[0]);
